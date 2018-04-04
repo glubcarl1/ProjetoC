3a9dca82c23744cf8df604ffe0c4b0fd66fdfda3
glubcarl1 <38057863+glubcarl1@users.noreply.github.com>
2018-04-04 00:19:37 -0300

    Inserindo código 4 no git.

---
+++ Fatorial/FatorialApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Variavel
+            int valor;
+
+            // Adiciona valor e guarda na variavel
+            Console.WriteLine("Entre com o valor:");
+            valor = int.Parse(Console.ReadLine()); // Tenta converter
+
+            // Chama o valor que o usuario digitou e mostra na tela
+            Console.WriteLine(Fatorial(valor)); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            Console.ReadKey(); //O ReadKey método espera, ou seja, blocos na emissão de thread de ReadKey método, até que uma tecla de função ou o caractere é pressionada.
+
+
+        }
+
+
+        // Função fatorial, Logica da operação
+        static int Fatorial( int i){
+            
+               if(i <= 1){
+                Fatorial = 1;
+
+               }else{
+                (i * Fatorial (i - 1)) ;
+                }
+
+          
+    }
+
+
+
+
+     }
+    }
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
