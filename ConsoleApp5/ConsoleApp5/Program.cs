d46ecb4c8264f98b8f2460fe1edbf341c7aef67d
glubcarl1 <38057863+glubcarl1@users.noreply.github.com>
2018-04-04 00:15:55 -0300

    Inserindo código 2 no git.

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis
+            double a, b, x;
+
+			// Inserir valores do usuário		
+			
+            Console.WriteLine("Entre com o valor de A:"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            a = double.Parse(Console.ReadLine()); // Double.Parse converte a representação de seqüência de caracteres de um número para o seu número de ponto flutuante de precisão dupla equivalente.
+            Console.WriteLine("Entre com o valor de B:"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            b = double.Parse(Console.ReadLine()); // Double.Parse converte a representação de seqüência de caracteres de um número para o seu número de ponto flutuante de precisão dupla equivalente.
+			
+			// A lógica da operação		
+            x = -b / a;
+			
+			// Mostrar o resultado na tela do usuário
+            Console.WriteLine(x); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            Console.ReadLine(); // ReadLine lê uma linha do fluxo de entrada padrão.
+        }
+    }
+}
