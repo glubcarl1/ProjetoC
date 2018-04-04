fc140e171d51fd12cb24fd9eecc73ac98199a453
glubcarl1 <38057863+glubcarl1@users.noreply.github.com>
2018-04-04 00:18:59 -0300

    Inserindo código 3 no git.

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//VER SE NUMERO É PAR OU IMPAR
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                   // VARIAVEIS
+            int N;
+            int Resultado_numero;
+            Console.WriteLine("escreva um nuemro desejado"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            N = int.Parse(Console.ReadLine()); // Tenta converter
+            Resultado_numero = N % 2;
+             // CONDIÇÃO DE PARA SER IMPAR
+            if (Resultado_numero == 1)
+            {
+                Console.WriteLine("Numero impar"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            }
+            // CONDIÇÃO PARA SER PAR
+            else
+            {
+                Console.WriteLine("Numero è par"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            };
+            // RESULTADO DA OPERAÇÃO
+            Console.WriteLine(Resultado_numero); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            Console.ReadLine(); // ReadLine lê uma linha do fluxo de entrada padrão.
+        }
+}
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Primeiro(string[] args)
+        {
+           // 10% do Numero esc
+
+            // Variaveis 
+            double A;
+            double Resultado;
+           
+
+            // Logica da operação
+            Console.WriteLine("Digite um valor"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            A = double.Parse(Console.ReadLine()); // Double.Parse converte a representação de seqüência de caracteres de um número para o seu número de ponto flutuante de precisão dupla equivalente.
+
+            Resultado = 0.1 *A;
+           
+            Console.WriteLine(Resultado); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            Console.ReadLine(); // ReadLine lê uma linha do fluxo de entrada padrão.
+        }
+    
+
+    static void Segundo(string[] args)
+    {
+        
+        // X% do Numero escolhido
+
+        // Variaveis 
+        double Porcentagem;
+        double N;
+        double Resultado;
+
+
+    
+            // Logica da operação
+            Console.WriteLine("Digite o valor da porcentagem"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            Porcentagem = double.Parse(Console.ReadLine()); // Double.Parse converte a representação de seqüência de caracteres de um número para o seu número de ponto flutuante de precisão dupla equivalente.
+
+            Console.WriteLine("Digite o valor da porcentagemdo numero"); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+            N = double.Parse(Console.ReadLine()); // Double.Parse converte a representação de seqüência de caracteres de um número para o seu número de ponto flutuante de precisão dupla equivalente.
+
+            Resultado = ((double)Porcentagem/100)*N;
+
+        Console.WriteLine(Resultado); // WriteLine grava o terminador de linha atual no fluxo de saída padrão.
+        Console.ReadLine(); // ReadLine lê uma linha do fluxo de entrada padrão.
+    }
+}
+}
